e8e42a2a166518baf91a5b78376d52cb5f1c506a
gonHid <gon.hidalgo.ch@gmail.com>
2026-07-22 22:35:23 -0400

    migration

--- Migrations/20260723023431_setIdlongToString.Designer.cs
+++ Migrations/20260723023431_setIdlongToString.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using BracketSmasherBackend.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace BracketSmasherBackend.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260723023431_setIdlongToString")]
+    partial class setIdlongToString
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Migrations/20260723023431_setIdlongToString.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BracketSmasherBackend.Migrations
+{
+    /// <inheritdoc />
+    public partial class setIdlongToString : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "SetId",
+                table: "MatchSessions",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(long),
+                oldType: "bigint");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "SetId",
+                table: "MatchResultReports",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(long),
+                oldType: "bigint");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<long>(
+                name: "SetId",
+                table: "MatchSessions",
+                type: "bigint",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<long>(
+                name: "SetId",
+                table: "MatchResultReports",
+                type: "bigint",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+        }
+    }
+}
